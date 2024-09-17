39d64808fec6f677751afbe1adf9f5653cc9df5d
Edgarskonnegard <j.edgarskonnegard@gmail.com>
2024-09-17 15:37:11 +0200

    löst felen i alla 6 fallen, lösningarna är kommenterade vid lämplig rad.

--- FelsökningInlämning4/Program.cs
+++ FelsökningInlämning4/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            /* Case 1
+            /* Case 1 --- klar
              *
             int number = 2;
 
@@ -18,21 +18,65 @@
             }
             */
 
-            /* Case 2
+            /* Case 2 --- klar
             for (int i = 1; i < 100; i++) // ändra till i <= 100
             {
                 Console.WriteLine(i);
             }
             */
 
-            for (int i = 1; i <= 5; i)
+            /*
+             * Case 3 --- klar
+            for (int i = 1; i <= 5; i++) // saknarr ++ ingen ökning 
             {
-                for (int j = 1; j <= i; j)
+                for (int j = 1; j <= i; j++) // saknarr ++ ingen ökning 
                 {
                     Console.Write(j + " ");
                 }
                 Console.WriteLine();
             }
+            */
+
+            /* Case 4 --- klar
+            int i = 1;
+            while (i <= 5)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write(""); // är tom, lägg till "" så funkar det.
+                }
+
+                Console.WriteLine(); // skriver ut en tom rad
+                i++;
+            }
+            */
+
+            /* 
+             * Case 5 --- klar
+            int i = 1;
+
+            switch (i)
+            {
+                case 1:// saknar "break;"
+                case 2:
+                    Console.WriteLine("Two");
+                    break;
+                default:
+                    Console.WriteLine("Other");
+                    break;
+            }
+            */
+           
+            /* Case 6 --- klar
+            int i = 10;
+
+            if (i = 5) // måste ha == för att verifiera 
+            {
+                Console.WriteLine("i är 5");
+            }
+            */
+
+
         }
     }
 }
