d823e64d2fca28f2e47ceea68988f9f0381cb1ed
Edgarskonnegard <j.edgarskonnegard@gmail.com>
2024-10-06 20:41:25 +0200

    Update Program.cs
    Created loop for testing cases.

--- FelsökningInlämning4/Program.cs
+++ FelsökningInlämning4/Program.cs
@@ -1,81 +1,116 @@
-﻿namespace FelsökningInlämning4
+// Edgar Skönnegård SUT24
+//
+// Skapade en loop för enklare koll om casen funkar.
+//
+namespace FelsökningInlämning4
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            /* Case 1 --- klar
-             *
-            int number = 2;
-
-            if (number > 3)
+            bool exitProgram = false;
+            while (!exitProgram)
             {
-                Console.WriteLine("Talet är större än tre") // saknar semikolon.
-            }
-            elseif(number < 3) // inget mellanslag i else if
-            {
-                Console.WriteLine("Talet är mindre än tre");
-            }
-            */
-
-            /* Case 2 --- klar
-            for (int i = 1; i < 100; i++) // ändra till i <= 100
-            {
-                Console.WriteLine(i);
-            }
-            */
-
-            /*
-             * Case 3 --- klar
-            for (int i = 1; i <= 5; i++) // saknarr ++ ingen ökning 
-            {
-                for (int j = 1; j <= i; j++) // saknarr ++ ingen ökning 
+                Console.WriteLine("Vilket fall vill du testa?");
+                Console.WriteLine("1" +
+                    "2" +
+                    "3" +
+                    "4" +
+                    "5" +
+                    "6" +
+                    "7: Exit program");
+                int choice;
+                try
                 {
-                    Console.Write(j + " ");
+                    choice = Convert.ToInt32(Console.ReadLine());
                 }
-                Console.WriteLine();
-            }
-            */
-
-            /* Case 4 --- klar
-            int i = 1;
-            while (i <= 5)
-            {
-                for (int j = 1; j <= i; j++)
+                catch
                 {
-                    Console.Write(""); // är tom, lägg till "" så funkar det.
+                    Console.WriteLine("invalid input");
+                }
+                switch (choice)
+                {
+                    case 1:
+                        int number = 2;
+
+                        if (number > 3)
+                        {
+                            // Saknade semikolon.
+                            Console.WriteLine("Talet är större än tre");
+                        }
+                        // saknade mellanslag i else if, (elseif).
+                        else if (number < 3)
+                        {
+                            Console.WriteLine("Talet är mindre än tre");
+                        }
+                        break;
+                    case 2:
+                        // Ändrade till i <= 100.
+                        for (int i = 1; i <= 100; i++)
+                        {
+                            Console.WriteLine(i);
+                        }
+                        break;
+                    case 3:
+                        // saknade ++ efter i, ingen ökning.
+                        for (int i = 1; i <= 5; i++)
+                        {
+                            // saknade ++ efter j, ingen ökning.
+                            for (int j = 1; j <= i; j++)
+                            {
+                                Console.Write(j + " ");
+                            }
+                            Console.WriteLine();
+                        }
+                        break;
+                    case 4:
+                        // Koden har inget output eftersom båda metoderna saknade argument.
+                        int i = 1;
+                        while (i <= 5)
+                        {
+                            for (int j = 1; j <= i; j++)
+                            {
+                                // Console.Write kräver ett argument annars blir det kompileringsfel.
+                                Console.Write("");
+                            }
+                            // Skriver ut en tom rad
+                            Console.WriteLine();
+                            i++;
+                        }
+                        break;
+                    case 5:
+                        int i = 1;
+
+                        switch (i)
+                        {
+                            case 1:
+                                // Saknades "break;"
+                                break;
+                            case 2:
+                                Console.WriteLine("Two");
+                                break;
+                            default:
+                                Console.WriteLine("Other");
+                                break;
+                        }
+                        break;
+                    case 6:
+                        int i = 10;
+                        // Saknades == för att verifiera, hade enbart '='. 
+                        if (i == 5)
+                        {
+                            Console.WriteLine("i är 5");
+                        }
+                        break;
+                    case 7:
+                        exitProgram = true;
+                        break;
+                    default:
+                        break;
                 }
 
-                Console.WriteLine(); // skriver ut en tom rad
-                i++;
             }
-            */
-
-            /* 
-             * Case 5 --- klar
-            int i = 1;
-
-            switch (i)
-            {
-                case 1:// saknar "break;"
-                case 2:
-                    Console.WriteLine("Two");
-                    break;
-                default:
-                    Console.WriteLine("Other");
-                    break;
-            }
-            */
            
-            /* Case 6 --- klar
-            int i = 10;
-
-            if (i = 5) // måste ha == för att verifiera 
-            {
-                Console.WriteLine("i är 5");
-            }
-            */
-
 
         }
     }
